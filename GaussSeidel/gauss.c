62aa7ac14e600ce8e681d0add2bbddf5cedd725c
kawamata <s16c1044sc@s.chibakoudai.jp>
2017-10-21 14:41:03 +0900

    関数化

--- GaussSeidel/gauss.c
+++ GaussSeidel/gauss.c
@@ -3,60 +3,32 @@
 #include <stdlib.h>
 #include<math.h>
 
-int main(void)
-{
-	char buffer[256];
-	char tok[] = "	";	
-	char *cor;
-	float **a;
-	float *b;
-	float com[2]={0, 0};
-	int i=0, j=0, k=0, N;
-	FILE *fp;
+void substitute_B(float **a, float *b, int *n){
 
-	fp = fopen("gauss_dataset.txt", "r");
-
-	if(fp == NULL){
-		printf("Can not find textfile\n");
-		return 0;
-	}
-	int n[2]={};	
-	float *x;
-
-	for(i=0; i<2; i++){
-		fgets(buffer, 255, fp);
-		sscanf(buffer, "%d", &n[i]);
-		if(n[i] < 1 || n[i] > 10){
-			printf("行列は1*1 ~ 10*10行列にて定義すること\n");
-			return 0;
-		}
-	}	
-
-	a = (float **)malloc(sizeof(float *) * n[0]);
-	b = (float *)malloc(n[0] * sizeof(float *));	
-	x = (float *)malloc(n[1] * sizeof(float *));
-
-	for(i=0; i<n[0]; i++){
-		a[i] = (float *)malloc((n[1]+1) * sizeof(float));
-	}
+	int i;
 	
-	i=j=0;
-	while((fgets(buffer, 255, fp)) != NULL){		
-		cor = strtok(buffer, tok);
-		while(cor != NULL){
-			sscanf(cor, "%f\t", &a[i][j]);
-			j++;
-			cor = strtok(NULL, tok);
-		}
-		i++;
-		j=0;
-	}	
-	fclose(fp);
-
 	for(i=0; i<n[0]; i++){
 		b[i] = a[i][n[1]];
 	}
-	
+	return;
+}
+
+void substitute_X(float *x, int *n){
+
+	int i;
+
+	for(i=0; i<n[1]; i++){
+		if(i == 0){
+			x[i] = 1.0;
+		}
+		x[i] = 0.0;
+	}
+	return;
+}
+void express_matrix(float **a, float *b, int *n){
+
+	int i, j;
+
 	printf("行列A= [\n");
 	for(i=0; i<n[0]; i++){
 		printf("\t[ ");
@@ -83,29 +55,13 @@ int main(void)
 		}
 		printf("\n\n");
 	}
+	return;
+}
 
-	float sum=0;
+float gauss(float **a, float *b, float *x, int *n){
 
-	/* 収束判定 */
-	for(i=0; i<n[0]; i++){
-		for(j=0; j<n[1]; j++){
-			if(i != j){
-				sum += a[i][j];
-			}
-		}	
-		if(fabsf(a[i][i]) <= fabsf(sum)){
-			printf("狭義対角優位行列ではないため, 収束しない\n");
-			return 0;
-		}
-		sum = 0;
-	}
-	printf("狭義対角優位行列より, 収束\n");
-
-	sum = 0;
-
-	for(i=0; i<n[1]; i++){
-		x[i] = 0.0;
-	}
+	int i, j, k=0;
+	float com[2]={0, 0}, sum;
 
 	do{
 		k++;
@@ -125,15 +81,89 @@ int main(void)
 			if(i == n[0]-1){
 				printf("\n");
 			}
-			if(k >= 100){
-				printf("収束せず\n");
-				return 0;
-			}
 		}
-
-        }while(fabsf(com[1] - com[0]) > 0.0001);	
-
+		if(k >= 100){
+			printf("収束せず\n");
+			break;
+		}
+	}while(fabsf(com[1] - com[0]) > 0.0001);
 	printf("収束に要した回数 : %d\n", k);
+	return;
+}
+
+void judgment_matrix(float **a, int *n){
+
+	float sum=0;	
+	int i, j;
+
+	for(i=0; i<n[0]; i++){
+		for(j=0; j<n[1]; j++){
+			if(i != j){
+				sum += a[i][j];
+			}
+		}	
+		if(fabsf(a[i][i]) <= fabsf(sum)){
+			printf("狭義対角優位行列ではないため, 収束しない可能性(%d列目)\n", i+1);
+		}
+		sum=0;
+	}
+	printf("狭義対角優位行列より, 収束可能性\n");
+	return;
+}
+
+int main()
+{
+	char buffer[256];
+	char tok[] = "	";	
+	char *cor;
+	float **a;
+	float *b;
+	int i, j;
+	FILE *fp;
+
+	fp = fopen("gauss_dataset.txt", "r");
+	if(fp == NULL){
+		printf("Can not find textfile\n");
+		return 0;
+	}
+	int n[2]={};	
+	float *x;
+
+	for(i=0; i<2; i++){
+		fgets(buffer, 255, fp);
+		sscanf(buffer, "%d", &n[i]);
+		if(n[i] < 1 || n[i] > 10){
+			printf("行列は1*1 ~ 10*10行列にて定義すること\n");
+			return 0;
+		}
+	}
+	
+	a = (float **)malloc(sizeof(float *) * n[0]);
+	b = (float *)malloc(n[0] * sizeof(float *));	
+	x = (float *)malloc(n[1] * sizeof(float *));
+
+	for(i=0; i<n[0]; i++){
+		a[i] = (float *)malloc((n[1]+1) * sizeof(float));
+	}
+	
+	i=j=0;
+	while((fgets(buffer, 255, fp)) != NULL){		
+		cor = strtok(buffer, tok);
+		while(cor != NULL){
+			sscanf(cor, "%f\t", &a[i][j]);
+			j++;
+			cor = strtok(NULL, tok);
+		}
+		i++;
+		j=0;
+	}	
+	fclose(fp);
+
+	substitute_B(a, b, n);
+	express_matrix(a, b, n);
+	judgment_matrix(a, n);
+	substitute_X(x, n);
+	gauss(a, b, x, n);
 	 
 	for(i=0; i<n[0]; i++){     
                 free(a[i]);
