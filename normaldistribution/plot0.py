83d369728662481d1b792c397f2f8faab3df7f4c
kawamata <s16c1044sc@s.chibakoudai.jp>
2017-10-23 23:55:20 +0900

    グラフ改良

--- normaldistribution/plot0.py
+++ normaldistribution/plot0.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
+#plt.figure()
+plt.subplots()
 
 x = np.arange(-20, 60, 0.1)
 mu = [0, 0, 0, 4, 44]
@@ -11,13 +12,13 @@ y04 = (1/np.sqrt(2*np.pi*si[1])) * np.exp(-(x-mu[1])**2/(2*si[1]))
 y09 = (1/np.sqrt(2*np.pi*si[2])) * np.exp(-(x-mu[2])**2/(2*si[2]))   
 y44 = (1/np.sqrt(2*np.pi*si[3])) * np.exp(-(x-mu[3])**2/(2*si[3]))   
 y444 = (1/np.sqrt(2*np.pi*si[4])) * np.exp(-(x-mu[4])**2/(2*si[4]))   
+S=3.75
 
-
-plt.plot(x, y01, color='red', label="N(0, 1)")
-plt.plot(x, y04, color='green', label="N(0, 4)")
-plt.plot(x, y09, color='blue', label="N(0, 9)")
-plt.plot(x, y44, color='indigo', label="N(4, 4)")
-plt.plot(x, y444, color='black', label="N(44, 4)")
+plt.plot(x, y01, color='red', linestyle="-", marker="8", markersize=S, label="N(0, 1)")
+plt.plot(x, y04, color='green', linestyle="-", marker="<", markersize=S, label="N(0, 4)")
+plt.plot(x, y09, color='blue', linestyle="-", marker=">", markersize=S, label="N(0, 9)")
+plt.plot(x, y44, color='indigo', linestyle="-", marker="s", markersize=S, label="N(4, 4)")
+plt.plot(x, y444, color='black', linestyle="-", marker="D", markersize=S, label="N(44, 4)")
 
 plt.legend()
 plt.grid()
