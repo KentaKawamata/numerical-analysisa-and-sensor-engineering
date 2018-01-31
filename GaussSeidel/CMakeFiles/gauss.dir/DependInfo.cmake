522c67eb068f0cea91df2f97b43f5aa1fee29f62
kawamata <s16c1044sc@s.chibakoudai.jp>
2018-02-01 01:41:56 +0900

    add CMakeLists

---
+++ GaussSeidel/CMakeFiles/gauss.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kawamata/program/4seme/GaussSeidel/gauss.c" "/home/kawamata/program/4seme/GaussSeidel/CMakeFiles/gauss.dir/gauss.c.o"
+  "/home/kawamata/program/4seme/GaussSeidel/gauss_calculate.c" "/home/kawamata/program/4seme/GaussSeidel/CMakeFiles/gauss.dir/gauss_calculate.c.o"
+  "/home/kawamata/program/4seme/GaussSeidel/gauss_express.c" "/home/kawamata/program/4seme/GaussSeidel/CMakeFiles/gauss.dir/gauss_express.c.o"
+  "/home/kawamata/program/4seme/GaussSeidel/gauss_judge.c" "/home/kawamata/program/4seme/GaussSeidel/CMakeFiles/gauss.dir/gauss_judge.c.o"
+  "/home/kawamata/program/4seme/GaussSeidel/gauss_serve.c" "/home/kawamata/program/4seme/GaussSeidel/CMakeFiles/gauss.dir/gauss_serve.c.o"
+  "/home/kawamata/program/4seme/GaussSeidel/gauss_substitute.c" "/home/kawamata/program/4seme/GaussSeidel/CMakeFiles/gauss.dir/gauss_substitute.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ GaussSeidel/CMakeFiles/gauss.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gauss.dir/gauss.c.o"
+  "CMakeFiles/gauss.dir/gauss_calculate.c.o"
+  "CMakeFiles/gauss.dir/gauss_express.c.o"
+  "CMakeFiles/gauss.dir/gauss_judge.c.o"
+  "CMakeFiles/gauss.dir/gauss_serve.c.o"
+  "CMakeFiles/gauss.dir/gauss_substitute.c.o"
+  "gauss.pdb"
+  "gauss"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gauss.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
